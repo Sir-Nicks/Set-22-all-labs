7997ff32a6646db88d5f6dd9da1d3604408af9a3
Nicolas Oluwamuyiwa Afolabi <nicofolabi25@gmail.com>
2024-11-19 10:49:40 +0100

    Jenkins Lab set 22

--- main.tf
+++ main.tf
@@ -1,182 +1,105 @@
-// This is custom VPC block
-resource "aws_vpc" "set-22-vpc" {
-  cidr_block = var.vpc_cidr
+// Use default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+// Public subnet 
+data "aws_subnets" "public_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+
+// Maven Instance
+resource "aws_instance" "maven" {
+  ami           = var.ami_red
+  instance_type = "t2.medium"
+  key_name      = aws_key_pair.key.id
+  vpc_security_group_ids = [aws_security_group.maven_sg.id]
+  associate_public_ip_address = true
+  user_data = file("./userdata.sh")
+}
+
+// Production Instance
+resource "aws_instance" "prod" {
+  ami           = var.ami_red
+  instance_type = "t2.medium"
+  key_name      = aws_key_pair.key.id
+  vpc_security_group_ids = [aws_security_group.prod_sg.id]
+  associate_public_ip_address = true
+  user_data = file("./userdata2.sh")
   tags = {
-    Name = "set-22-vpc"
+    Name = "host-server"
   }
-}
-
-// This is public subnet 1 block
-resource "aws_subnet" "set-22-pub-sub-1" {
-  vpc_id     = aws_vpc.set-22-vpc.id
-  cidr_block = var.public_subnet_1_cidr
-
-  tags = {
-    Name = "set-22-pub-sub-1"
-    Type = "Public"
-  }
-}
-
-// This is public subnet 2 block
-resource "aws_subnet" "set-22-pub-sub-2" {
-  vpc_id     = aws_vpc.set-22-vpc.id
-  cidr_block = var.public_subnet_2_cidr
-
-  tags = {
-    Name = "set-22-pub-sub-2"
-    Type = "Public"
-  }
-}
-
-// This is private subnet 1 block
-resource "aws_subnet" "set-22-prv-sub-1" {
-  vpc_id     = aws_vpc.set-22-vpc.id
-  cidr_block = var.private_subnet_1_cidr
-
-  tags = {
-    Name = "set-22-prv-sub-1"
-    Type = "Private"
-  }
-}
-
-// This is private subnet 2 block
-resource "aws_subnet" "set-22-prv-sub-2" {
-  vpc_id     = aws_vpc.set-22-vpc.id
-  cidr_block = var.private_subnet_2_cidr
-
-  tags = {
-    Name = "set-22-prv-sub-2"
-    Type = "Private"
-  }
-}
-
-// This is the Internet Gateway block
-resource "aws_internet_gateway" "set-22-igw" {
-  vpc_id = aws_vpc.set-22-vpc.id
-
-  tags = {
-    Name = "set-22-igw"
-  }
-}
-
-// Elastic IP for NAT Gateway
-resource "aws_eip" "set-22-eip" {
-  domain = "vpc"
-}
-
-// NAT Gateway
-resource "aws_nat_gateway" "set-22-nat" {
-  allocation_id = aws_eip.set-22-eip.id
-  subnet_id     = aws_subnet.set-22-pub-sub-1.id
-
-  tags = {
-    Name = "set-22-nat"
-  }
-}
-
-// Public Route Table
-resource "aws_route_table" "set-22-public-rt" {
-  vpc_id = aws_vpc.set-22-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.set-22-igw.id
-  }
-
-  tags = {
-    Name = "set-22-public-rt"
-  }
-}
-
-// Route Table Association for Public Subnet 1
-resource "aws_route_table_association" "set-22-public-rt-assoc-1" {
-  subnet_id      = aws_subnet.set-22-pub-sub-1.id
-  route_table_id = aws_route_table.set-22-public-rt.id
-}
-
-// Route Table Association for Public Subnet 2
-resource "aws_route_table_association" "set-22-public-rt-assoc-2" {
-  subnet_id      = aws_subnet.set-22-pub-sub-2.id
-  route_table_id = aws_route_table.set-22-public-rt.id
 }
 
 // Keypair
-resource "aws_key_pair" "set-22-key" {
-  key_name   = "set-22-key"
-  public_key = file("./ec2.pub")
+resource "aws_key_pair" "key" {
+  key_name   = "demo_key"
+  public_key = tls_private_key.key.public_key_openssh
 }
 
-// Security group
-resource "aws_security_group" "ansible-sg" {
-  name        = "ansible-sg"
-  description = "Security group for Ansible"
+// Maven Security Group
+resource "aws_security_group" "maven_sg" {
+  name        = "maven_sg"
+  description = "Security group for Maven instance"
   ingress {
-    description = "Allow SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Production Security Group
+resource "aws_security_group" "prod_sg" {
+  name        = "prod_sg"
+  description = "Security group for production server"
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "Allow HTTP access"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  }
-  tags = {
-    Name = "ansible-sg"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-// Ansible Control Node Ubuntu
-resource "aws_instance" "ansible_server" {
-  ami                    = var.ami_ubuntu
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ansible-sg.id]
-  key_name               = aws_key_pair.set-22-key.key_name
-  associate_public_ip_address = true
-  user_data = file("./user_data.sh")
-
-  tags = {
-    Name = "ansible-control-node"
-  }
+// RSA key generation
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-// Ubuntu Managed Node block
-resource "aws_instance" "ubuntu_server" {
-  ami                    = var.ami_ubuntu
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ansible-sg.id]
-  key_name               = aws_key_pair.set-22-key.key_name
-  associate_public_ip_address = true
-  user_data = file("./user_data.sh")
-
-  tags = {
-    Name = "ubuntu-managed-node"
-  }
+// Private key local storage
+resource "local_file" "key" {
+  content        = tls_private_key.key.private_key_pem
+  filename       = "demo-key.pem"
+  file_permission = 600
 }
 
-// RedHat Managed Node block
-resource "aws_instance" "redhat_server" {
-  ami                    = var.ami_red
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ansible-sg.id]
-  key_name               = aws_key_pair.set-22-key.key_name
-  associate_public_ip_address = true
-  user_data              = file("./user_data.sh")
-
-  tags = {
-    Name = "redhat-managed-node"
-  }
+output "maven_ip" {
+  value = aws_instance.maven.public_ip
 }
 
-
-
+output "prod_ip" {
+  value = aws_instance.prod.public_ip
+}
--- variable.tf
+++ variable.tf
@@ -1,40 +1,4 @@
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_1_cidr" {
-  description = "The CIDR block for the first public subnet"
-  default     = "10.0.1.0/24"
-}
-
-variable "public_subnet_2_cidr" {
-  description = "The CIDR block for the second public subnet"
-  default     = "10.0.2.0/24"
-}
-
-variable "private_subnet_1_cidr" {
-  description = "The CIDR block for the first private subnet"
-  default     = "10.0.3.0/24"
-}
-
-variable "private_subnet_2_cidr" {
-  description = "The CIDR block for the second private subnet"
-  default     = "10.0.4.0/24"
-}
-
-variable "port_number" {
-  description = "The port number for SSH access"
-  default     = 22
-}
-
-variable "ami_ubuntu" {
-  description = "The AMI ID for the Ubuntu instances"
-  default     = "ami-0d64bb532e0502c46" // Valid for eu-west-1
-}
-
 variable "ami_red" {
-  description = "The AMI ID for the RedHat instances"
-  default     = "ami-07d4917b6f95f5c2a" // Valid RedHat AMI in eu-west-1
+  description = "The AMI ID for RedHat"
+  default     = "ami-07d4917b6f95f5c2a"
 }
-
