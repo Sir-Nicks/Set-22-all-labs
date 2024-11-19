22843a13892cd74282680bcb5eff49e4f9c5f6de
Nicolas Oluwamuyiwa Afolabi <nicofolabi25@gmail.com>
2024-11-19 10:50:31 +0100

    Docker Lab set 22

--- main.tf
+++ main.tf
@@ -1,105 +1,131 @@
-// Use default VPC
-data "aws_vpc" "default" {
-  default = true
+// aws console
+provider "aws" {
+  region = "eu-west-1"
 }
 
-// Public subnet 
-data "aws_subnets" "public_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
+resource "aws_subnet" "default_subnet" {
+  vpc_id                  = "vpc-0f7a985c4a51dd38f"
+  cidr_block              = "172.31.1.0/24" # Adjusted to fit default VPC range
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-1a"
 
-
-// Maven Instance
-resource "aws_instance" "maven" {
-  ami           = var.ami_red
-  instance_type = "t2.medium"
-  key_name      = aws_key_pair.key.id
-  vpc_security_group_ids = [aws_security_group.maven_sg.id]
-  associate_public_ip_address = true
-  user_data = file("./userdata.sh")
-}
-
-// Production Instance
-resource "aws_instance" "prod" {
-  ami           = var.ami_red
-  instance_type = "t2.medium"
-  key_name      = aws_key_pair.key.id
-  vpc_security_group_ids = [aws_security_group.prod_sg.id]
-  associate_public_ip_address = true
-  user_data = file("./userdata2.sh")
   tags = {
-    Name = "host-server"
+    Name = "default-subnet"
   }
 }
 
-// Keypair
-resource "aws_key_pair" "key" {
-  key_name   = "demo_key"
-  public_key = tls_private_key.key.public_key_openssh
-}
-
-// Maven Security Group
-resource "aws_security_group" "maven_sg" {
-  name        = "maven_sg"
-  description = "Security group for Maven instance"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-// Production Security Group
-resource "aws_security_group" "prod_sg" {
-  name        = "prod_sg"
-  description = "Security group for production server"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-// RSA key generation
-resource "tls_private_key" "key" {
+// RSA key of size 4096 bits
+resource "tls_private_key" "keypair" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-// Private key local storage
-resource "local_file" "key" {
-  content        = tls_private_key.key.private_key_pem
-  filename       = "demo-key.pem"
-  file_permission = 600
+// Creating private keypair file locally
+resource "local_file" "private_key" {
+  content         = tls_private_key.keypair.private_key_pem
+  filename        = "docker-key.pem"
+  file_permission = "600"
 }
 
-output "maven_ip" {
-  value = aws_instance.maven.public_ip
+// Creating EC2 key pair
+resource "aws_key_pair" "keypair" {
+  key_name   = "docker-key"
+  public_key = tls_private_key.keypair.public_key_openssh
 }
 
-output "prod_ip" {
-  value = aws_instance.prod.public_ip
+// Security group for Docker
+resource "aws_security_group" "docker_sg" {
+  name        = "docker-sg"
+  description = "Allow inbound traffic for Docker instance"
+
+  ingress {
+    description = "Allow application traffic"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "docker-sg"
+  }
+}
+
+// Creating EC2 instance for Docker
+resource "aws_instance" "docker_host" {
+  ami                    = "ami-0d64bb532e0502c46" // Red Hat AMI
+  instance_type          = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+  subnet_id              = aws_subnet.default_subnet.id
+  key_name               = aws_key_pair.keypair.key_name
+  associate_public_ip_address = true
+  user_data              = file("./docker-userdata.sh")
+
+  tags = {
+    Name = "docker-host"
+  }
+}
+
+// Security group for Maven
+resource "aws_security_group" "maven_sg" {
+  name        = "maven-sg"
+  description = "Security group for Maven instance"
+
+  ingress {
+    description = "Allow SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "maven-sg"
+  }
+}
+
+// Creating EC2 instance for Maven
+resource "aws_instance" "maven_host" {
+  ami                    = "ami-0d64bb532e0502c46" // Red Hat AMI
+  instance_type          = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.maven_sg.id]
+  subnet_id              = aws_subnet.default_subnet.id
+  key_name               = aws_key_pair.keypair.key_name
+  associate_public_ip_address = true
+  user_data              = file("./maven-userdata.sh")
+
+  tags = {
+    Name = "maven-host"
+  }
+}
+
+// Outputs
+output "docker-ip" {
+  value = aws_instance.docker_host.public_ip
+}
+
+output "maven-ip" {
+  value = aws_instance.maven_host.public_ip
 }
